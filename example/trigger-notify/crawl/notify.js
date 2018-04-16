e0cd9e7dcf84913511825e78434c9a65a0cb361a
Yoohogyun <yoohoogun114@naver.com>
2018-04-16 21:18:16 +0900

    Migrated completed Notify Slack

--- example/trigger-notify/crawl/notify.js
+++ example/trigger-notify/crawl/notify.js
@@ -8,7 +8,8 @@ const request = require("request");
 // Const Variables
 const CRAWAL_START_YEAR = 2008;
 const CRAWAL_END_YEAR = new Date().getFullYear;
-const SLACK_TARGET = "https://hooks.slack.com/services/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";
+const SLACK_TARGET =
+  "https://hooks.slack.com/services/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";
 
 module.exports.notify = (event, context, callback) => {
   // 모든 키워드 가져와서 키워드대로 분류
@@ -19,61 +20,76 @@ module.exports.notify = (event, context, callback) => {
   };
 
   // write the todo to the database
-  dynamoDb
-    .scan(params, (error, data) => {
-      if (error) {
-        console.error(error);
-        callback(null, {
-          statusCode: error.statusCode || 501,
-          headers: { "Content-Type": "text/plain" },
-          body: "Couldn't Scan the table"
-        });
-        return;
+  dynamoDb.scan(params, (error, data) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { "Content-Type": "text/plain" },
+        body: "Couldn't Scan the table"
+      });
+      return;
+    }
+
+    let items = data["Items"];
+
+    // data 분류
+    items.forEach(item => {
+      let keyword = item.keyword; // 김치
+      if (!keywordObj.hasOwnProperty(keyword)) {
+        // key 없으면 만들고
+        keywordObj[keyword] = [item];
+      } else {
+        // key 있으면 추가
+        keywordObj[keyword].push(item);
       }
-
-      let items = data["Items"];
-
-      // data 분류
-      items.forEach(item => {
-        let keyword = item.keyword; // 김치
-        if (!keywordObj.hasOwnProperty(keyword)) {
-          // key 없으면 만들고
-          keywordObj[keyword] = [item.id];
-        } else {
-          // key 있으면 추가
-          keywordObj[keyword].push(item.id);
-        }
-      });
-      console.log("keywordObj is", keywordObj);
-
-      // Delete Item
-      let removeQueryPromiseArr = Object.keys(keywordObj).map(keyword => {
-        let handleArr = keywordObj[keyword];
-        let handleArrLength = handleArr.length || 0;
-        if (handleArrLength >= CRAWAL_END_YEAR - CRAWAL_START_YEAR + 1) {
-          // currently 2008 ~ 2018 => 11 year
-          // Slack to Dev Chanel
-          slack_noti(keyword);
-
-          return handleArr.map(id => {
-            const params = {
-              Key: {
-                id: {
-                  S: id
-                },
-                TableName: process.env.DYNAMODB_TABLE
-              }
-            };
-
-            return dynamoDb.deleteItem(params).promise();
-          });
-        }
-      });
-
-      Promise.all(removeQueryPromiseArr).then(() => {
-        callback(null, "Success");
-      });
     });
+    console.log("keywordObj is", keywordObj);
+
+    /*
+      keywordObj is { '치킨': 
+      [ { years: '2010', keyword: '치킨' },
+        { years: '2011', keyword: '치킨' } ] }
+      */
+
+    // Delete Item
+    let removeQueryPromiseArr = Object.keys(keywordObj).map(keyword => {
+      let handleArr = keywordObj[keyword];
+      let handleArrLength = handleArr.length || 0;
+      if (handleArrLength >= CRAWAL_END_YEAR - CRAWAL_START_YEAR + 1) {
+        // currently 2008 ~ 2018 => 11 year
+        // Slack to Dev Chanel
+        slack_noti(keyword);
+
+        let toDeleteItemsArr = [];
+        handleArr.forEach(item => {
+          let toDeleteItem = {
+            DeleteRequest: {
+              Key: item
+            }
+          };
+          toDeleteItemsArr.push(toDeleteItem);
+        });
+
+        var deleteParams = {
+          RequestItems: {
+            [process.env.DYNAMODB_TABLE]: toDeleteItemsArr
+          }
+        };
+
+        dynamoDb.batchWrite(deleteParams, function(err, data) {
+          if (err) {
+            console.log("Batch delete unsuccessful ...");
+            console.log(err, err.stack); // an error occurred
+          } else {
+            console.log("Batch delete successful ...");
+            console.log(data); // successful response
+          }
+          callback(null, "Success");
+        });
+      }
+    });
+  });
 };
 
 function slack_noti(keyword) {
@@ -90,36 +106,8 @@ function slack_noti(keyword) {
     },
     json: true
   };
-  request(options, function (error, response, body) {
+  request(options, function(error, response, body) {
     if (error) throw new Error(error);
     console.log(body);
   });
 }
-
-var params = {
-  TableName: process.env.DYNAMODB_TABLE,
-  KeyConditionExpression: "keyword = :a",
-  ExpressionAttributeValues: {
-      ":a": "치킨",
-  }
- };
- 
- dynamoDb.query(params, function(err, data) {
-   if (err) console.log(err, err.stack); // an error occurred
-   else     console.log(data);           // successful response
-   /*
-   data = {
-    ConsumedCapacity: {
-    }, 
-    Count: 2, 
-    Items: [
-       {
-      "SongTitle": {
-        S: "Call Me Today"
-       }
-     }
-    ], 
-    ScannedCount: 2
-   }
-   */
- });
